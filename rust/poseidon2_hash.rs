81f0fd1f8c7a6474f4c7bae9c066e9431cbd5e05
zsyyjs <117369230+zsyyjs@users.noreply.github.com>
2025-07-14 04:56:04 +0800

    Update poseidon2_hash.rs

--- rust/poseidon2_hash.rs
+++ rust/poseidon2_hash.rs
@@ -1,22 +1,27 @@
 use poseidon_rs::poseidon2::{Poseidon2, Fr};
 use std::fs::File;
 use std::io::Write;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Input {
+    x: Vec<Fr>,
+    hash: String,
+}
 
 fn main() {
-    let inputs = vec![
-        Fr::from(123456789u64),
-        Fr::from(987654321u64),
-    ];
+    let inputs = vec![Fr::from(123456789u64), Fr::from(987654321u64)];
     let poseidon = Poseidon2::new(2);
     let hash = poseidon.hash(inputs.clone());
 
-    let mut file = File::create("../inputs/input.json").unwrap();
-    write!(
-        file,
-        "{{\n  \"x\": [{}, {}],\n  \"hash\": \"{}\"\n}}",
-        inputs[0], inputs[1], hash
-    )
-    .unwrap();
+    let input_struct = Input {
+        x: inputs,
+        hash: format!("{}", hash),
+    };
+
+    let json_str = serde_json::to_string_pretty(&input_struct).unwrap();
+    let mut file = File::create("inputs/input.json").unwrap();
+    file.write_all(json_str.as_bytes()).unwrap();
 
     println!("输入与哈希结果已写入 inputs/input.json");
 }
